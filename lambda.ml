aca9a01fdd0b94288f6fa22280f5ae73c34b1bb5
Jordan Scales <scalesjordan@gmail.com>
2019-07-07 01:55:16 +0000

    use the original pred from wikipedia

--- lambda.ml
+++ lambda.ml
@@ -126,15 +126,18 @@ let mult m n = abstr ["f"; "x"] (
         ; Variable "x"
         ]
   )
-let pred n =
-  let f = Abstraction
-      ( "p"
-      , let sp = second (Variable "p")
-        in pair sp (succ sp)
+let pred n = abstr ["f"; "x"] (
+    let ux = Abstraction ("u", Variable "x")
+    and uu = Abstraction ("u", Variable "u")
+    and inner = abstr ["g"; "h"] (
+        Application ( Variable "h"
+                    , Application ( Variable "g"
+                                  , Variable "f"
+                                  )
+                    )
       )
-  and zero = abstr ["f"; "x"] (Variable "x")
-  in let pc0 = pair zero zero
-  in first (app [n; f; pc0])
+    in app [n; inner; ux; uu]
+  )
 
 let () =
   let two = succ (succ zero)
@@ -150,7 +153,7 @@ let () =
   in (
     assert (0 = (app [zero; f; nil] |> len_of_lambda_list));
     assert (2 = (app [two; f; nil] |> len_of_lambda_list));
-    assert (4 = (app [pred (add two three); f; nil] |> len_of_lambda_list));
+    assert (5 = (app [add two three; f; nil] |> len_of_lambda_list));
     assert (6 = (app [mult two three; f; nil] |> len_of_lambda_list));
-    assert (49 = (app [mult seven seven; f; nil] |> len_of_lambda_list));
+    assert (48 = (app [pred (mult seven seven); f; nil] |> len_of_lambda_list));
   )
